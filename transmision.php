eb322b878db25dfc4ae1a7c87471482d1d258758
ItaloOriel <icarvajal903@outlook.com>
2023-11-22 20:02:39 -0300

    Update transmision.php

--- transmision.php
+++ transmision.php
@@ -37,8 +37,6 @@
         #messages {
             list-style: none;
             padding: 0;
-            overflow-y: scroll;
-            max-height: 300px; /* Ajusta según tus necesidades */
         }
 
         form {
@@ -62,54 +60,25 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js"></script>
     <script>
-        // Configuración de Firebase (reemplaza con tu propia configuración)
-        var firebaseConfig = {
-            apiKey: "AIzaSyDNbT7HesuApTeNz06vwh3PPxvSpmF-kg4",
-            authDomain: "chat-poyecto.firebaseapp.com",
-            projectId: "chat-poyecto",
-            storageBucket: "chat-poyecto.appspot.com",
-            messagingSenderId: "274196426167",
-            appId: "1:274196426167:web:67628d8edd7a7e80404c32",
-            measurementId: "G-NF961Q2D1K"
-            };
-
-        // Inicializar Firebase
-        firebase.initializeApp(firebaseConfig);
-
-        // Referencia a la base de datos de Firebase
-        var database = firebase.database();
-
         $(document).ready(function () {
-            var chatRef = database.ref('chat');
-
-            function actualizarChat(snapshot) {
-                // Obtener los mensajes desde Firebase
-                var mensajes = snapshot.val();
-                $('#messages').empty();
-                mensajes.forEach(function (mensaje) {
-                    $('#messages').append('<li>' + mensaje + '</li>');
+            function actualizarChat() {
+                // Obtener mensajes desde el archivo
+                $.get('obtener_mensajes.php', function (data) {
+                    $('#messages').html(data);
                 });
-
-                // Desplazar el contenedor del chat hacia abajo para mostrar los mensajes más recientes
-                $('#messages').scrollTop($('#messages')[0].scrollHeight);
             }
-
-            // Escuchar cambios en la base de datos (nuevos mensajes)
-            chatRef.on('value', actualizarChat);
 
             // Manejar el envío de mensajes desde el formulario
             $('#form').submit(function () {
-                // Obtener el mensaje del campo de entrada
-                var mensaje = $('#m').val();
-
-                // Enviar el mensaje a Firebase
-                chatRef.push(mensaje);
-
-                // Limpiar el campo de entrada después de enviar el mensaje
-                $('#m').val('');
+                $.post('enviar_mensaje.php', { mensaje: $('#m').val() }, function (response) {
+                    if (response.status === 'success') {
+                        // Limpiar el campo de entrada después de enviar el mensaje
+                        $('#m').val('');
+                        // Actualizar el chat después de enviar un mensaje
+                        actualizarChat();
+                    }
+                }, 'json');
 
                 return false;
             });
@@ -123,6 +92,9 @@
                 .catch(function (error) {
                     console.error('Error al acceder a la cámara:', error);
                 });
+
+            // Actualizar el chat cada 2 segundos (puedes ajustar este intervalo según tus necesidades)
+            setInterval(actualizarChat, 2000);
         });
     </script>
 
